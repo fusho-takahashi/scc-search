c91b23025e74eae8ab3d4fd175dfbf152939f6a4
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-12-31 13:54:30 +0900

    list item に matRipple を追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatRippleModule } from '@angular/material/core';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoItemComponent } from './components/video-item/video-item.component';
 
 @NgModule({
   declarations: [AppComponent, VideoListComponent, VideoItemComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatListModule, MatChipsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatListModule, MatChipsModule, MatRippleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
