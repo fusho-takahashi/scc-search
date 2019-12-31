eb2f4baf8b21ac7ac968feae424961c241d42e93
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-12-31 15:47:48 +0900

    sort の実装

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { VIDEO_DATA } from './VIDEO_DATA';
 import { VideoData } from '../shared/domain/model';
 
+type sortOrder = '新しい順' | '古い順' | '再生数が多い順';
+
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
@@ -9,14 +11,46 @@ import { VideoData } from '../shared/domain/model';
 })
 export class VideoListComponent {
   videoData: VideoData[] = [...VIDEO_DATA];
+  order: sortOrder = '新しい順';
+
   constructor() {}
 
   sortNewer() {
-    this.videoData = [...VIDEO_DATA];
+    this.videoData.sort((a, b) => {
+      if (a.id > b.id) {
+        return -1;
+      }
+      if (a.id < b.id) {
+        return 1;
+      }
+      return 0;
+    });
+    this.order = '新しい順';
   }
 
   sortOlder() {
-    this.videoData = [...VIDEO_DATA];
-    this.videoData.reverse();
+    this.videoData.sort((a, b) => {
+      if (a.id < b.id) {
+        return -1;
+      }
+      if (a.id > b.id) {
+        return 1;
+      }
+      return 0;
+    });
+    this.order = '古い順';
+  }
+
+  sortViewCount() {
+    this.videoData.sort((a, b) => {
+      if (a.viewCount > b.viewCount) {
+        return -1;
+      }
+      if (a.viewCount < b.viewCount) {
+        return 1;
+      }
+      return 0;
+    });
+    this.order = '再生数が多い順';
   }
 }
