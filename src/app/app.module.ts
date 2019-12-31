9f1438c07f259d6cb60abc7964d6f8d8bc08b75d
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-12-31 14:53:17 +0900

    並べ替えボタンを表示

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,26 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatRippleModule } from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoItemComponent } from './components/video-item/video-item.component';
 
 @NgModule({
   declarations: [AppComponent, VideoListComponent, VideoItemComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatListModule, MatChipsModule, MatRippleModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatChipsModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
