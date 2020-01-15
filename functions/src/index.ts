37a417fa0ff7dcd5707fba6dbff043bf6baa7e34
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-01-15 19:37:07 +0900

    用意したvideo info を restore するfunctionを作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { VIDEO_DATA } from './VIDEO_DATA';
+import { VideoInfo } from '../../src/app/shared/domain/model';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.restoreVideoInfo = functions.https.onRequest(async (req, res) => {
+  VIDEO_DATA.forEach(async (info: VideoInfo) => {
+    await db.collection('videos').add(info);
+  });
+  res.status(200).send('success');
+});
